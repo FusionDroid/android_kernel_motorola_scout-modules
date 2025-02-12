MotorolaMobilityLLC/kernel-kernel_device_modules-6.1

Reconfig prefer snk/src according to source support APDO
Moto 68W charger using private protocol and
Check the cable sid before communication.
So need config prefer src for reading sid
Using whether the source support APDO
For checking which cable need prefer snk/src.

Change-Id: I3d05f6c6759e8c359fdf532446c92f21ddac66c5
Signed-off-by: huwenbo <wingt122@motorola.com>
Reviewed-on: https://gerrit.mot.com/3052879
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Tested-by: Jira Key
Reviewed-by: Wei Xu <xuwei9@lenovo.com>
Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
Submit-Approved: Jira Key

drivers/misc/mediatek/typec/tcpc/pd_dpm_core.c
@@ -98,7 +98,7 @@ static void pd_dpm_update_pdos_flags(struct pd_port *pd_port, uint32_t pdo,
 	pd_port->pe_data.dpm_flags = dpm_flags;
 #ifdef CONFIG_SUPPORT_MMI_ADAPTER
 	DPM_INFO("pd_dpm_update_pdos_flags,flag:%d,dpm_flag:%d,dpm_caps:%d\n",dynamic_dpm_caps,pd_port->pe_data.dpm_flags,pd_port->dpm_caps);
-	if(dynamic_dpm_caps && (pdo & PDO_FIXED_COMM_CAP)) {
+	if(dynamic_dpm_caps && ((pdo & PDO_FIXED_COMM_CAP) || !pd_is_source_support_apdo(pd_port))) {
 		pd_port->dpm_caps &= ~DPM_CAP_DR_CHECK_PROP(2);
 		DPM_INFO("pd_dpm_update_pdos_flags, dpm_caps:%d\n",pd_port->dpm_caps);
 	}

FusionDroid/android_kernel_motorola_scout-modules

drivers/misc/mediatek/typec/tcpc/pd_dpm_core.c
@@ -101,7 +101,7 @@ static void pd_dpm_update_pdos_flags(struct pd_port *pd_port, uint32_t pdo,
 	pd_port->pe_data.dpm_flags = dpm_flags;
 #ifdef CONFIG_SUPPORT_MMI_ADAPTER
 	DPM_INFO("pd_dpm_update_pdos_flags,flag:%d,dpm_flag:%d,dpm_caps:%d\n",dynamic_dpm_caps,pd_port->pe_data.dpm_flags,pd_port->dpm_caps);
-	if(dynamic_dpm_caps && (pdo & PDO_FIXED_COMM_CAP)) {
+	if(dynamic_dpm_caps && ((pdo & PDO_FIXED_USB_COMM) || !pd_is_source_support_apdo(pd_port))) {
 		pd_port->dpm_caps &= ~DPM_CAP_DR_CHECK_PROP(2);
 		DPM_INFO("pd_dpm_update_pdos_flags, dpm_caps:%d\n",pd_port->dpm_caps);
 	}